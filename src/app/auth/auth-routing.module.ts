f90fb064bf66c0999bfcc6bb4cb20178cfa6601b
Javier <javier.sanchez28@protonmail.com>
2023-01-26 00:11:23 -0600

    Creacion del formulario de registro

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,15 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 
-
 const routes: Routes = [
   {
     path: '',
-    children: [
+    children : [
       {path: 'register' , component: RegisterComponent},
       {path: 'login' , component: LoginComponent},
-      {path: '**' , redirectTo: 'login'}
+      {path: '**' , redirectTo: 'register'}
     ]
+
+
   }
 ];
 
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+
+  //TODO: TEMP
+
+  nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  miForm: FormGroup = this.fb.group(
+    {
+      nombre: ['', [Validators.required , Validators.pattern(this.nombreApellidoPattern)]],
+      email: ['', [Validators.required , Validators.pattern(this.emailPattern)]],
+    }
+  )
+
+  campoNoValido(campo: string){
+    return this.miForm.get(campo)?.invalid &&
+      this.miForm.get(campo)?.touched;
+  }
+
+
+
+  constructor(private fb: FormBuilder) {
+    this.miForm.reset( {
+      nombre: 'Javier Sanchez',
+    })
+  }
+
+
+  guardar(){
+    console.log(this.miForm.value);
+    this.miForm.markAsTouched();
+  }
+
 
 }
